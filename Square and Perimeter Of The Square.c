0c60b9849e9bc4702dccaa02bfb51c90e0a62ed6
JoelJohnsonThomas <83132383+JoelJohnsonThomas@users.noreply.github.com>
2021-05-07 12:08:45 +0530

    Create Square and Perimeter Of The Square.c

---
+++ Square and Perimeter Of The Square.c
@@ -0,0 +1,11 @@
+# include<stdio.h>
+int main()
+{
+   float a,Area,Perimeter;
+   printf("Enter The Side Of The Square:\n");
+   scanf("%f",&a);
+   Area = a * a;
+   Perimeter = 4 * a;
+   printf("The Area Of The Square : %f\n",Area);
+   printf("The Perimeter Of The Square : %f\n",Perimeter);
+} 

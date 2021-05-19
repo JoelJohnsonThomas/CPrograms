6f8d00f91d500a115b53fb119248b23c8b1d9a4b
JoelJohnsonThomas <83132383+JoelJohnsonThomas@users.noreply.github.com>
2021-05-19 08:39:37 +0530

    Create Conversion Of Celsius To Kelvin.c

---
+++ Conversion Of Celsius To Kelvin.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+int main()
+{
+    printf("Hello Programmer,We are going to convert celsius to kelvin\n");
+    int i,Kelvin;
+    printf("Enter The Degree Of Celsius:\n");
+    scanf("%d",&i);
+    Kelvin = i + 272;
+    printf("The Degree Of kelvin:%dk\n",Kelvin);
+    return 0;
+
+
+}

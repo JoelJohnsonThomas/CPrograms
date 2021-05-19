dc83f509490681a314ddac23426e35e1526649cb
JoelJohnsonThomas <83132383+JoelJohnsonThomas@users.noreply.github.com>
2021-05-19 08:43:57 +0530

    Create Checking Whether The Given Number Is Even Or Not.c

---
+++ Checking Whether The Given Number Is Even Or Not.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter Any Number:\n");
+    scanf("%d",& n);
+    if (n%2 ==0)
+        printf("The Given Number Is Even\n");
+    else
+        printf("The Given Number Is Odd\n");
+    
+    
+}
